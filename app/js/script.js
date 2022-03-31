6fa9e2e9109e0e842f46bd263a9b5ab8ab60950c
Leo Morgan <laumorgan.14@gmail.com>
2022-03-31 13:42:03 +0100

    added animation to button & attribution div

--- app/js/script.js
+++ app/js/script.js
@@ -2,6 +2,10 @@ const API_LINK = "https://api.adviceslip.com/advice"
 const slip_number = document.getElementById('advice-number');
 const slip_quote = document.getElementById('quote');
 const dice_button = document.getElementById('dice-btn');
+const dice_img = document.getElementById('dice');
+const vector = document.getElementById('vector');
+const attr_text = document.getElementById('attr_text');
+const vector_div = document.getElementById('vector_div');
 
 
 const fetchAdvice = async () => {
@@ -24,5 +28,37 @@ const generateNewAdvice = async () => {
 };
 
 window.addEventListener('DOMContentLoaded', () => {
-    dice_button.addEventListener('click', generateNewAdvice)
+    dice_button.addEventListener('click', generateNewAdvice);
+});
+
+dice_button.addEventListener('click', function(){
+    dice_img.classList.toggle('spin');
+    setTimeout(() => {
+        dice_img.classList.remove('spin')
+    }, 900);
+});
+
+vector_div.addEventListener('click', function(){
+
+    if(attr_text.classList.contains('hidden')){
+        attr_text.classList.remove('hidden');
+        attr_text.classList.toggle('fade-in');
+        attr_text.classList.remove('fade-out');
+    } else {
+        attr_text.classList.remove('fade-in');
+        attr_text.classList.toggle('fade-out');
+        setTimeout(() => {
+            attr_text.classList.toggle('hidden')
+        }, 1500);
+    }
 })
+
+const Rotate = () => {
+    if (vector_div.classList.contains("normal")) {
+        vector_div.classList.add("rotate");
+        vector_div.classList.remove("normal");
+    } else if (vector_div.classList.contains("rotate")){
+        vector_div.classList.add("normal");
+        vector_div.classList.add("rotate");
+    }
+}
